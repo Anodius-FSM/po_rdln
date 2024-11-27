192181c1be93a786342838104e64ad25c224721a
tfo <tfo@tfo.com>
2024-11-27 15:09:41 +0100

    back to exifjs

--- gps_extractor.js
+++ gps_extractor.js
@@ -36,7 +36,7 @@ const gps_extractor = (() => {
 
         (async () => {
             const imageBlob =  await common.fetchPhotoV2({description: '29EBF883B53158BA6AFF91A06EB02285', id: '29EBF883B53158BA6AFF91A06EB02285', type: 'JPEG'});
-            reader.readAsArrayBuffer(imageBlob);  
+            reader.readAsDataURL(blob);  /** readAsArrayBuffer(imageBlob); */   
         })()
     }
 
